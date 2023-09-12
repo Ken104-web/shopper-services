272841ba69ab5ababc983b1d9dfeb472eae27cf8
KenCorp <kennethwarui35@gmail.com>
2023-09-13 00:35:56 +0300

    call customer by id

--- cli.py
+++ cli.py
@@ -13,20 +13,31 @@ while True:
     def cli():
         "Thank you for shopping at Ujenzi Supermarket"
 
-    
-
-
+  
     @cli.command()
     # calling customer commandlist
     def display_customers():
-        click.echo(f'Here is a lis of customer: ')
+        click.echo(f'Here is a list of customer: ')
         list_customers = session.query(Customer).all()
         for customer in list_customers:
                 click.echo(f"{customer.id}, Name: {customer.name}")
 
+   
+        def call_customer(id):
+            # call a specific customer
+            customer = session.query(Customer).filter_by(id=id).first()
+            if customer: 
+                click.echo(f"Name of customer: {customer.name}")
+            else:
+                click.echo(f"Customer not found")
+                
+        name = click.prompt('\nEnter the name of the customer you want to call:')
+        call_customer(name)
 
+    
+
+    
     break
-
 
 if __name__ == '__main__':
     cli()
